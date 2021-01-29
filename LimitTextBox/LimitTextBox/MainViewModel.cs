73cf70061e54551565032f35d88d416f4d66e2fb
김기운 <rldnsdl123@naver.com>
2021-01-29 12:09:11 +0900

    Textbox 내용을 제어하는코드

---
+++ LimitTextBox/LimitTextBox/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace LimitTextBox
+{
+    public class MainViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+        private string _Box;
+        public string Box
+        {
+            get => _Box;
+            set
+            {
+                value= CheckString(value);
+                if (_Box==value)
+                    return;
+
+                _Box = value;
+                OnPropertyChanged(nameof(Box));
+            }
+        }
+
+        public MainViewModel()
+        {
+        }
+
+        private string CheckString(string msg)
+        {
+            List<string> LimitText = new List<string>
+            {
+                "<",
+                ">",
+                "\'",
+                "\"",
+                "&",
+            };
+
+            foreach(string entity in LimitText)
+            {
+                if (msg.Contains(entity))
+                {
+                    msg = msg.Replace(entity, "");
+                    System.Diagnostics.Debug.WriteLine(entity + "삭제");
+                }
+            }
+
+            return msg;
+        }
+    }
+}
